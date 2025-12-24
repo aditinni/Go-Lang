7bce48d63512838f53518392d6892b1a32ad489c
Aditya.R_11 <raj378388@gmail.com>
2025-12-24 11:35:38 +0530

    Maps in GoLang

---
+++ Maps/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var colors map[string]string
+
+	colors := make(map[string]string)
+
+	/*	colors := map[string]string{
+		"red":   "#FF0000",
+		"green": "#00FF00",
+		"blue":  "#0000FF",
+	} */
+
+	colors["white"] = "#FFFFFF"
+	colors["black"] = "#000000"
+	colors["yellow"] = "#FFFF00"
+	delete(colors, "white")
+
+	//iterate over map
+	printMap(colors)
+
+	fmt.Println(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
